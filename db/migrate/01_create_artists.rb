84cf3ebcbd1ff583dd204483ac2f63b1bae41720
Irum <irum@Irums-MacBook-Air.local>
2018-10-01 10:06:06 +0100

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,7 +2,8 @@ class CreateArtists < ActiveRecord::Migration[4.2]
 
   def change
     create_table :artists do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
     end
-  end 
+  end
 end
